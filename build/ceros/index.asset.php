58e5e9346d9077b6acfd99fb3cd2ec91559ee54b
Alireza Sabouri <alireza.sabouri@ceros.com>
2026-07-31 16:46:35 +0200

    exclude './build/' dir from git

--- build/ceros/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '7e0a51a0786a2b24af9a');
